bc07c51b2fb4b75c8e2ccae41d6f84450df1f9d3
amandayu255 <amandayu255@gmail.com>
2025-07-07 18:06:54 -0700

    added petImage function and variable

---
+++ app/src/main/java/com/codepath/randompet/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.codepath.randompet
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.codepath.asynchttpclient.callback.TextHttpResponseHandler
+import okhttp3.Headers
+
+class MainActivity : AppCompatActivity() {
+    var petImageURL = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        getDogImageURL()
+    }
+
+    private fun getDogImageURL() {
+        val client = AsyncHttpClient()
+
+        client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                petImageURL = json.jsonObject.getString("message")
+                Log.d("Dog", "response successful$json")
+                Log.d("petImageURL", "pet image URL set")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Dog Error", errorResponse)
+            }
+        }]
+    }
+}
